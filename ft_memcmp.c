420797e1470fa83effa64f99eaab25c19aeb715c
Junichi Tanaka <junichi200123@gmail.com>
2020-10-06 05:22:17 +0900

    fix: ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/06 02:36:40 by jtanaka           #+#    #+#             */
-/*   Updated: 2020/10/06 02:50:41 by jtanaka          ###   ########.fr       */
+/*   Updated: 2020/10/06 05:20:58 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	str1 = (char*)s1;
 	str2 = (char*)s2;
 	i = 0;
-	while (i < (int)n && !(*str1 == '\0' && *str2 == '\0')){
+	while (i < (int)n && (*str1 || *str2)){
 		if (*str1 != *str2)
 			return (*str1 - *str2);
 		str1++;
