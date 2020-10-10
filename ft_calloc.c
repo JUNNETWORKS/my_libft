07dfb9ac495d7bf0b7b7ce08f4ddd5a2380506e8
Junichi Tanaka <junichi200123@gmail.com>
2020-10-11 02:14:15 +0900

    fix calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 03:48:56 by jtanaka           #+#    #+#             */
-/*   Updated: 2020/10/10 19:19:42 by jtanaka          ###   ########.fr       */
+/*   Updated: 2020/10/11 02:13:48 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,10 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	unsigned char	*allocation;
-	unsigned int	i;
 
 	allocation = malloc(nmemb * size);
 	if (allocation == NULL)
 		return (NULL);
-	i = 0;
-	while (i < (unsigned int)size)
-		allocation[i] = 0;
+	ft_memset(allocation, 0, nmemb * size);
 	return (void *)allocation;
 }
