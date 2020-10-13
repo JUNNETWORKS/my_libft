f013383b794c6a3bd4a863b6a0411818c06bfc23
Junichi Tanaka <junichi200123@gmail.com>
2020-10-14 04:15:51 +0900

    WIP: ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/12 22:03:24 by jtanaka           #+#    #+#             */
-/*   Updated: 2020/10/12 22:22:54 by jtanaka          ###   ########.fr       */
+/*   Updated: 2020/10/13 02:52:58 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,24 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
-
-	t_list	*new_lst_tmp;
+	t_list	*new_elem;
 
 	new_lst = malloc(sizeof(t_list));
 	new_lst->content = f(lst->content);
 	lst = lst->next;
 	while (lst->next != NULL)
 	{
-		new_lst_tmp->content = f(lst->content);
-		new_lst_tmp->next = malloc(sizeof(t_list));
-		new_lst_tmp = new_lst_tmp->next;
+		new_elem->content = f(lst->content);
+		if (new_elem->content == NULL)
+		{
+			lst = lst->next;
+			continue;
+		}
+		new_elem->next = malloc(sizeof(t_list));
+		new_elem = new_elem->next;
+		lst = lst->next;
 	}
-	new_lst->content = f(lst->content);
-	new_lst->next = NULL;
+	new_elem->content = f(lst->content);
+	new_elem->next = NULL;
 	return (new_lst);
 }
-
