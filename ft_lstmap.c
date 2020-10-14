3c0cb6bd2908fb2b0a2b739b13af912ee3b71c4f
Junichi Tanaka <junichi200123@gmail.com>
2020-10-15 02:10:04 +0900

    lstmap

---
+++ ft_lstmap.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/14 23:48:01 by jtanaka           #+#    #+#             */
+/*   Updated: 2020/10/15 02:09:29 by jtanaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_lst;
+	t_list	*new_elem;
+
+	if (lst == NULL || f == NULL)
+		return (NULL);
+	if ((new_elem = ft_lstnew(f(lst->content))) == NULL)
+	{
+		ft_lstclear(&lst, del);
+		return (NULL);
+	}
+	new_lst = new_elem;
+	lst = lst->next;
+	while (lst)
+	{
+		if ((new_elem = ft_lstnew(f(lst->content))) == NULL)
+		{
+			ft_lstclear(&new_lst, del);
+			ft_lstclear(&lst, del);
+			break ;
+		}
+		ft_lstadd_back(&new_lst, new_elem);
+		lst = lst->next;
+	}
+	return (new_lst);
+}
