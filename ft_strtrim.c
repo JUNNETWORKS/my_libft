b2ecc332ba838d870c60c48a66ceed9e5793185b
Junichi Tanaka <junichi200123@gmail.com>
2020-10-15 03:06:05 +0900

    fix: strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/11 03:19:24 by jtanaka           #+#    #+#             */
-/*   Updated: 2020/10/11 04:48:49 by jtanaka          ###   ########.fr       */
+/*   Updated: 2020/10/15 03:04:26 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int		in_set(char c, char *set)
 {
+	if (!set)
+		return (0);
 	while (*set)
 	{
 		if (c == *set)
@@ -27,27 +29,27 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	char			*str;
 	int				idx;
-	int				start;
-	int				end;
+	int				start_idx;
+	int				last_idx;
 
-	if (*s1 == '\0')
-		return ("");
+	if (s1 == NULL || *s1 == '\0')
+		return (ft_calloc(sizeof(char), 1));
 	idx = 0;
-	start = 0;
-	end = ft_strlen(s1) - 1;
+	start_idx = 0;
+	last_idx = ft_strlen(s1) - 1;
 	while (in_set(s1[idx], (char*)set))
-		start = ++idx;
+		start_idx = ++idx;
 	idx = ft_strlen(s1) - 1;
 	while (in_set(s1[idx], (char*)set))
-		end = --idx;
-	if (start >= end)
-		return ("");
-	str = malloc(end - start + 2);
+		last_idx = --idx;
+	if (start_idx >= last_idx)
+		return (ft_calloc(sizeof(char), 1));
+	str = malloc(last_idx - start_idx + 2);
 	if (str == NULL)
 		return (NULL);
 	idx = 0;
-	while (start <= end)
-		str[idx++] = s1[start++];
+	while (start_idx <= last_idx)
+		str[idx++] = s1[start_idx++];
 	str[idx] = '\0';
 	return (str);
 }
