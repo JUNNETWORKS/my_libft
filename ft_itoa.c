2b188da1cb217b53098fd6513df56cc1e85d2fbf
Junichi Tanaka <junichi200123@gmail.com>
2021-04-02 18:49:05 +0900

    fix: norm error in ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,44 +6,56 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/12 02:42:43 by jtanaka           #+#    #+#             */
-/*   Updated: 2020/12/16 09:15:54 by jtanaka          ###   ########.fr       */
+/*   Updated: 2021/04/02 18:47:55 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int		get_digitll(long long n)
+static	int	get_digit(int n)
 {
 	int		digit;
 
-	digit = 1;
-	while (n /= 10)
+	if (n == 0)
+		return (1);
+	digit = 0;
+	while (n)
+	{
 		digit++;
+		n /= 10;
+	}
 	return (digit);
 }
 
-char			*ft_itoa(int n)
+static	int	itoa_recursive(char *result, int n)
 {
-	int			allocation_size;
-	int			digit;
-	char		*result;
-	int			idx;
-	long long	nll;
+	int	idx;
+	int	sign;
 
-	nll = (long long)n;
-	digit = get_digitll(nll);
-	allocation_size = nll < 0 ? digit + 2 : digit + 1;
-	if (!(result = (char*)malloc(allocation_size)))
-		return (result);
-	result[allocation_size - 1] = '\0';
-	result[0] = nll < 0 ? '-' : '0';
-	nll *= nll < 0 ? -1 : 1;
-	idx = allocation_size - 2;
-	while (nll)
-	{
-		result[idx] = '0' + (nll % 10);
-		nll /= 10;
-		idx--;
-	}
+	idx = 0;
+	sign = 1;
+	if (n / 10)
+		idx = itoa_recursive(result, n / 10);
+	else if (n < 0)
+		result[idx++] = '-';
+	if (n < 0)
+		sign = -1;
+	result[idx++] = '0' + sign * (n % 10);
+	result[idx] = '\0';
+	return (idx);
+}
+
+char	*ft_itoa(int n)
+{
+	int			arr_size;
+	char		*result;
+
+	arr_size = get_digit(n) + 1;
+	if (n < 0)
+		arr_size++;
+	result = (char *)ft_calloc(arr_size, sizeof(char));
+	if (!result)
+		return (NULL);
+	itoa_recursive(result, n);
 	return (result);
 }
